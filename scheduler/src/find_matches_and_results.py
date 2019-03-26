1dc7efeb660853509f648c2871aaf77e86d8f091
0bart <barti1002@gmail.com>
2019-03-26 19:31:54 +0000

    DB split for fixtures and results

--- scheduler/src/find_matches_and_results.py
+++ scheduler/src/find_matches_and_results.py
@@ -14,7 +14,8 @@ with open('config.json') as file_conf:
 db_string = 'mongodb://{}:{}@{}:{}'.format(conf['mongo']['user'], conf['mongo']['pwd'], conf['mongo']['host'], conf['mongo']['port'])
 
 client = pymongo.MongoClient(db_string)
-matches = client.matches.betMatches
+fixtures = client.betapp.fixtures
+results = client.betapp.results
 
 def convert_team_name(nameTeam):
     nameTeam = nameTeam.replace(' FC', '')
@@ -25,17 +26,17 @@ def convert_team_name(nameTeam):
 
 
 def predict_result(odd_H, odd_A, odd_D, prob_down=0.17, prob_up=0.36):
-    p1 = 1 / (1 + odd_H/odd_A + odd_H/odd_D)
-    p2 = 1 / (1 + odd_A/odd_H + odd_A/odd_D)
-    pX = 1 / (1 + odd_D/odd_A + odd_D/odd_H)
+    p1 = round(1 / (1 + odd_H/odd_A + odd_H/odd_D), 4)
+    p2 = round(1 / (1 + odd_A/odd_H + odd_A/odd_D), 4)
+    pX = round(1 / (1 + odd_D/odd_A + odd_D/odd_H), 4)
     if math.fabs(p1 - p2) <= prob_down:
-        return 'DRAW'
+        return p1, p2, pX, 'DRAW'
     elif (p1 - p2) >= prob_up:
-        return 'HOME_TEAM'
+        return p1, p2, pX, 'HOME_TEAM'
     elif (p2 - p1) >= prob_up:
-        return 'AWAY_TEAM'
+        return p1, p2, pX, 'AWAY_TEAM'
     elif math.fabs(p1 - p2) > prob_down and math.fabs(p1 - p2) < prob_up:
-        return 'SKIP'
+        return p1, p2, pX, 'SKIP'
 
 
 def find_matches():
@@ -55,36 +56,43 @@ def find_matches():
         match_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(match['commence_time']))
         home_team = match['home_team']
         index_ = match['teams'].index(home_team)
-        away_team = match['teams'][1-index_]
+        #away_team = match['teams'][1-index_]
         away_team = [team for team in match['teams'] if team != home_team][0]
-        odd1_H, odd1_A, odd1_D, odd1_name = match['sites'][0]['odds']['h2h'][index_], match['sites'][0]['odds']['h2h'][1-index_], match['sites'][0]['odds']['h2h'][2], match['sites'][0]['site_nice']
+        bets = []
+        probs = []
+        for bookmaker in match['sites']:
+            odd1_H, odd1_A, odd1_D, odd1_name = bookmaker['odds']['h2h'][index_], bookmaker['odds']['h2h'][1-index_], bookmaker['odds']['h2h'][2], bookmaker['site_nice']
+            p1, p2, pX, prediction = predict_result(odd1_H, odd1_A, odd1_D)
+            _bets = {
+                "bet_name": odd1_name,
+                "bet_home": odd1_H,
+                "bet_draw": odd1_A,
+                "bet_away": odd1_D
+            }
+            _probs = {
+                "prediction": prediction,
+                "p1": p1,
+                "p2": p2,
+                "pX": pX
+            }
+            bets.append(_bets)
+            probs.append(_probs)
         hashs = "{}, {} - {}".format(match_time, home_team, away_team)
         match_hash = hashlib.md5(hashs.encode('utf-8')).hexdigest()[-16:]
-        prediction = predict_result(odd1_H, odd1_A, odd1_H)
 
         doc = {
               'match_time': datetime.datetime.utcfromtimestamp(match['commence_time']).replace(tzinfo=datetime.timezone.utc),
               'home_team': home_team,
               'away_team': away_team,
-              'bet': {
-                  'bet_name': odd1_name,
-                  'bet_home': odd1_H,
-                  'bet_away': odd1_A,
-                  'bet_draw': odd1_D,
-                  },
-              'prediction': prediction,
-              'score': {
-                  'score_home': None,
-                  'score_away': None,
-                  'result': None,
-                  },
+              'bets': bets,
+              'probabilities': probs,
               'match_hash': match_hash,
               'last_updated': datetime.datetime.now()
               }
 
-        result = matches.update_one({'match_hash': match_hash}, {'$set': doc}, upsert = True)
-        matched_count += result.matched_count
-        modified_count += result.modified_count
+        update_one_output = fixtures.update_one({'match_hash': match_hash}, {'$set': doc}, upsert=True)
+        matched_count += update_one_output.matched_count
+        modified_count += update_one_output.modified_count
 
     return 
 
@@ -94,15 +102,18 @@ def find_results():
     uri = conf['find_results']['uri']
     api_key = conf['find_results']['api_key']
 
-    headers = { 'X-Auth-Token': api_key }
+    headers = {'X-Auth-Token': api_key}
     
-    results = requests.get(uri, headers=headers)
-    results = results.json()
+    #result = requests.get(uri, headers=headers)
+    #result = result.json()
+    
+    with open("data.json", "r") as resultx:
+        result = json.load(resultx)
 
     matched_count = 0
     modified_count = 0
     
-    for item in results['matches']:
+    for item in result['matches']:
         match_time = item['utcDate'].replace('T', ' ')[0:-1]
         home_team = convert_team_name(item['homeTeam']['name'])
         away_team = convert_team_name(item['awayTeam']['name'])
@@ -118,9 +129,14 @@ def find_results():
                 'last_updated': datetime.datetime.now(),
                 }
 
-        result = matches.update_one({'match_hash': match_hash}, {'$set':  doc})
-        matched_count += result.matched_count
-        modified_count += result.modified_count
+        find_one_output = fixtures.find_one({'match_hash': match_hash})
+        doc.update(find_one_output)
+
+        #delete_one_output = fixtures.delete_one({'match_hash': match_hash})
+
+        update_one_output = results.update_one({'match_hash': match_hash}, {'$set': doc}, upsert=True)
+        matched_count += update_one_output.matched_count
+        modified_count += update_one_output.modified_count
 
     return
 
@@ -132,3 +148,7 @@ while True:
     schedule.run_pending()
     print("I am in while loop")
     time.sleep(5)
+#if __name__ == '__main__':
+#    find_matches()
+#    time.sleep(5)
+#    find_results()
